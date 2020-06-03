8c3f7c9d8d5ebd4f5e439dfd85e51b0e861ff954
ZXZCNPOWER(SCHOOL) <49693076+ZXZCNPOWER@users.noreply.github.com>
2020-06-03 21:51:08 +0800

    Update exam_service_server.py

--- final_exam/exam_service_server.py
+++ final_exam/exam_service_server.py
@@ -1,1 +1,18 @@
+#! /usr/bin/env python
 
+import rospy                                      # the main module for ROS-python programs
+from std_srvs.srv import Trigger, TriggerResponse # we are creating a 'Trigger service'...
+                                                  # ...Other types are available, and you can create
+                                                  # custom types
+def trigger_response(request):
+
+    return TriggerResponse(
+        success=True,
+        message="front"
+    )
+ 
+rospy.init_node('exam_service')                     # initialize a ROS node
+my_service = rospy.Service(                        # create a service, specifying its name,
+    '/exam_service', Trigger, trigger_response         # type, and callback
+)
+rospy.spin()   
